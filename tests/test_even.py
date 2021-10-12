d6785f370532f1c539c8625239c7682f3f7a6ded
Anastasiia Shishova <shishova@arrival.com>
2021-10-12 12:50:39 +0300

    trailing whitespace fix

--- tests/test_even.py
+++ tests/test_even.py
@@ -10,7 +10,7 @@ def test_get_game_data():
     data = even.get_game_data()
 
     assert len(data) == 2
-    assert type(data['task']) == int 
+    assert type(data['task']) == int
     assert type(data['correct_answer']) == str
     assert data['correct_answer'] == 'yes' or data['correct_answer'] == 'no'
 
